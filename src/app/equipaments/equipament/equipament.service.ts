cf0955a76041cb90213d62c56e9c51c7aca3281f
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-09-07 12:49:02 +0200

    no message

--- src/app/equipaments/equipament/equipament.service.ts
+++ src/app/equipaments/equipament/equipament.service.ts
@@ -13,25 +13,13 @@ export class EquipamentService {
 
   	constructor(private http: Http) { }
 
-  	crearEquipament(id, nom, municipi, comarca, categories){
-
-  		const headers = new Headers();
-  		headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-  		//return this.http.post()
-  	}
-
     // L L I S T A R   T O T S   E L S   R E G I S T R E S   D E   L A   B B D D
   	getAll(query, page, itemsperpage) {
-  		// console.log("IPP: " + itemsperpage);
   		page = isNaN(page) ? 1 : page;
   		query = query ? "&filter=" + query : "";
-  		// return this.http.get(this.apiurl + "?rpp="+itemsperpage+"&page="+page + query/*, {withCredentials:true}*/);
-  		// return this.http.get(this.apiurl + "?rpp=10"+"&page="+page + query/*, {withCredentials:true}*/);
-  		
-  		// return this.http.get(this.apiurl + "?query=&page=1&rpp=10"/*, {withCredentials:true}*/);
+
   		return this.http
-  				   .get(this.apiurl + "?rpp=" + itemsperpage + "&page=1&query=")
+  				   .get(this.apiurl + "?rpp=" + itemsperpage + "&page=" + page + query)
   				   .map(res => res.json());
   	}
 
@@ -47,43 +35,15 @@ export class EquipamentService {
 
     // M O D I F I C A R   U N   E Q U I P A M E N T
     update(equipament) {
-      console.log(JSON.stringify(equipament));
-      
-      //let headers = new Headers({ 'Content-Type': 'application/json' });
-      //let options = new RequestOptions({ headers: headers });
-
-      //var headers2 = new Headers();
-      //headers2.append('Access-Control-Allow-Origin', '*'); 
-
       let headers = new Headers();
       headers.append('Content-Type', 'application/json');
 
       return this.http.put(this.apiurl + equipament.id, equipament, {headers:headers}).map(res => res.json());
-      //return this.http.put(this.apiurl + equipament.id, equipament, options).map(res => res.json());
-                 
-                 //.map(res => res.json);
     }
 
     // E S B O R R A R   U N   E Q U I P A M E N T
     del(equipament) {
-
+      return this.http.delete(this.apiurl + equipament.id)
+                 .map(res => res.text());
     }
 }
-
-//add: function (equipament) { 
-//            return $http.post(apiurl, JSON.stringify(equipament)/*, {withCredentials:true}*/); 
-//        },
-
-/*
-crearProfe(id, nom, cognoms, dni, curs, naixement) {
-
-        const creds = 'idc=' + curs + '&idp=' + id + '&nomp=' + nom + '&cognoms=' + cognoms + '&dni=' + dni + '&naixement=' + naixement;
-
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/x-www-form-urlencoded');
-
-        return this.http.post(this.myURL + this.urlCrearProfe, creds, {
-            headers: headers
-        }).map(res => {console.log("la resposta és: " + res.json()), res.json()})
-    }
-*/
